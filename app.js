02186e95196e4b13095612f9231f8c34aeff4bdc
SirGhock <joaofernandogarciadesouza@hotmail.com>
2019-10-31 23:04:11 -0300

    adjust port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let express = require('express')
 let app = express()
-let port = 3000
+let port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`)
   console.log('Para derrubar o servidor: ctrl + c');
