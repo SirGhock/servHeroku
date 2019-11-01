3e948e0a6155603761d377c0e389cc8772d49a4c
SirGhock <joaofernandogarciadesouza@hotmail.com>
2019-10-31 23:01:30 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+let express = require('express')
+let app = express()
+let port = 3000
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`)
+  console.log('Para derrubar o servidor: ctrl + c');
+})
+
+app.get('/', function (req, res) {
+    res.send('Pagina teste pedro');
+  });
